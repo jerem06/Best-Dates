208379da8d234174598a18a8612182d15606a078
jbencini <jbencini@afdtech.com>
2021-08-22 10:04:09 +0200

    Initial Commit

---
+++ Shared/DatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatesApp.swift
+//  Shared
+//
+//  Created by Jérémie BENCINI on 22/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct DatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
